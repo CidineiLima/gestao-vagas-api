6fb23435bb51bb6f4e9ca07efcc5114ce1eaca96
Cidinei-Lima <cidinei.lima@hotmail.com>
2025-09-13 20:50:58 -0300

    add candidate controller, service, repository, entity

---
+++ src/main/java/br/cidinei/lima/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,24 @@
+package br.cidinei.lima.gestao_vagas.modules.candidate.useCases;
+
+import br.cidinei.lima.gestao_vagas.exceptions.UserFoundException;
+import br.cidinei.lima.gestao_vagas.modules.candidate.CandidateEntity;
+import br.cidinei.lima.gestao_vagas.modules.candidate.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+
+	@Autowired
+	private CandidateRepository candidateRepository;
+	public CandidateEntity execute(CandidateEntity candidateEntity){
+		this.candidateRepository
+				.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+				.ifPresent((user) -> {
+					throw new UserFoundException();
+				});
+
+		this.candidateRepository.save(candidateEntity);
+		return candidateEntity;
+	}
+}

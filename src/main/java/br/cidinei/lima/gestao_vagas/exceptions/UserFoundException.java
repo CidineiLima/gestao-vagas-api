decced8866b79ce21851e2a0aa0ef373a52637b2
Cidinei-Lima <cidinei.lima@hotmail.com>
2025-09-13 23:23:21 -0300

    Add entity, Controller, service e repository Job

--- src/main/java/br/cidinei/lima/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/br/cidinei/lima/gestao_vagas/exceptions/UserFoundException.java
@@ -2,6 +2,6 @@ package br.cidinei.lima.gestao_vagas.exceptions;
 
 public class UserFoundException extends RuntimeException{
 	public UserFoundException(){
-		super("Usuário já existe");
+		super("Cadastro já existe");
 	}
 }

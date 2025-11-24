78a7f74384986fd0094fa4679d252751f0973654
Cidinei-Lima <cidinei.lima@hotmail.com>
2025-11-23 22:02:09 -0300

    Add swagger para documentação

--- src/main/java/br/cidinei/lima/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/cidinei/lima/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,19 @@
 package br.cidinei.lima.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 import br.cidinei.lima.gestao_vagas.modules.candidate.CandidateEntity;
+import br.cidinei.lima.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
+import br.cidinei.lima.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
+import br.cidinei.lima.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.access.prepost.PreAuthorize;
 import br.cidinei.lima.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import br.cidinei.lima.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
@@ -13,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
 public class CandidateController {
 	@Autowired
 	private CreateCandidateUseCase createCandidateUseCase;
@@ -20,7 +33,17 @@ public class CandidateController {
 	@Autowired
 	private ProfileCandidateUseCase profileCandidateUseCase;
 
+	@Autowired
+	private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
+
 	@PostMapping("/")
+	@Operation(summary = "Cadastro de candidato", description = "Essa função é responsável por cadastrar um candidato")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", content = {
+					@Content(schema = @Schema(implementation = CandidateEntity.class))
+			}),
+			@ApiResponse(responseCode = "400", description = "Usuário já existe")
+	})
 	public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
 		try {
 			var result = createCandidateUseCase.execute(candidateEntity);
@@ -32,6 +55,14 @@ public class CandidateController {
 
 	@GetMapping("/")
 	@PreAuthorize("hasRole('CANDIDATE')")
+	@Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", content = {
+					@Content(schema = @Schema(implementation = ProfileCandidateResponseDTO.class))
+			}),
+			@ApiResponse(responseCode = "400", description = "User not found")
+	})
+	@SecurityRequirement(name = "jwt_auth")
 	public ResponseEntity<Object> get(HttpServletRequest request) {
 		var idCandidate = request.getAttribute("candidate_id");
 
@@ -42,6 +73,18 @@ public class CandidateController {
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+	@GetMapping("/job")
+	@PreAuthorize("hasRole('CANDIDATE')")
+	@Operation(summary = "Listagem de vagas disponível para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro")
+	@SecurityRequirement(name = "jwt_auth")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", content = {
+					@Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+			})
+	})
+	public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+		return this.listAllJobsByFilterUseCase.execute(filter);
 	}
 }
 
--- src/main/java/br/cidinei/lima/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/cidinei/lima/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,8 +1,12 @@
 package br.cidinei.lima.gestao_vagas.modules.company.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -10,19 +14,27 @@ import java.util.UUID;
 
 @Entity(name = "job")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
+
+	@Schema(example = "Vaga para design")
 	private String description;
+
+	@Schema(example = "GymPass, Plano de saúde")
 	private String benefits;
 
 	@NotBlank(message = "Esse campo é obrigatório")
+	@Schema(example = "SENIOR")
 	private String level;
 
 	@ManyToOne()
-	@JoinColumn(name="company_id", insertable = false, updatable = false)
+	@JoinColumn(name = "company_id", insertable = false, updatable = false)
 	private CompanyEntity companyEntity;
 
 	@Column(name = "company_id", nullable = false)

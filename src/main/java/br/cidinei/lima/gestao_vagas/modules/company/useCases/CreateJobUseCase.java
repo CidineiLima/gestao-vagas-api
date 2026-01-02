cfd80f0a22d3fac8361dcfaee5a4079fa871fa08
Cidinei-Lima <cidinei.lima@hotmail.com>
2026-01-02 18:13:27 -0300

    JobController: Add caso de testes

--- src/main/java/br/cidinei/lima/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/br/cidinei/lima/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -1,6 +1,8 @@
 package br.cidinei.lima.gestao_vagas.modules.company.useCases;
 
+import br.cidinei.lima.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.cidinei.lima.gestao_vagas.modules.company.entities.JobEntity;
+import br.cidinei.lima.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.cidinei.lima.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +13,13 @@ public class CreateJobUseCase {
 	@Autowired
 	private JobRepository jobRepository;
 
-	public JobEntity execute(JobEntity jobEntity){
-	return this.jobRepository.save(jobEntity);
+	@Autowired
+	private CompanyRepository companyRepository;
+
+	public JobEntity execute(JobEntity jobEntity) {
+		companyRepository.findById(jobEntity.getCompanyId()).orElseThrow(() -> {
+			throw new CompanyNotFoundException();
+		});
+		return this.jobRepository.save(jobEntity);
 	}
 }
